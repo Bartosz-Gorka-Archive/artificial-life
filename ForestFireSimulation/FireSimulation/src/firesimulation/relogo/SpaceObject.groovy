784a1776066a060dea7eb8fd9f9fa4d287ba4c40
Bartosz Górka <bartosz.gorka@appunite.com>
2019-10-27 08:53:29 +0100

    Initialize World with trees, empty space and fire

---
+++ ForestFireSimulation/FireSimulation/src/firesimulation/relogo/SpaceObject.groovy
@@ -0,0 +1,16 @@
+package firesimulation.relogo
+
+import static repast.simphony.relogo.Utility.*
+import static repast.simphony.relogo.UtilityG.*
+
+import firesimulation.ReLogoPatch
+import repast.simphony.relogo.Plural
+import repast.simphony.relogo.Stop
+import repast.simphony.relogo.Utility
+import repast.simphony.relogo.UtilityG
+import repast.simphony.relogo.schedule.Go
+import repast.simphony.relogo.schedule.Setup
+
+class SpaceObject extends ReLogoPatch {
+
+}
