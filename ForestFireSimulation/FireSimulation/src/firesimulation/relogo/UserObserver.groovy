9fa0fdd5c7200b4b91e0ee815b09e3f45571c5e3
Bartosz Górka <bartosz.gorka@interia.eu>
2019-10-27 10:55:40 +0100

    Reorganize Fire simulation (#2)
    * Reorganize Fire simulation

--- ForestFireSimulation/FireSimulation/src/firesimulation/relogo/UserObserver.groovy
+++ ForestFireSimulation/FireSimulation/src/firesimulation/relogo/UserObserver.groovy
@@ -1,48 +1,32 @@
 package firesimulation.relogo
 
-import static repast.simphony.relogo.Utility.*;
-import static repast.simphony.relogo.UtilityG.*;
-import repast.simphony.relogo.Stop;
-import repast.simphony.relogo.Utility;
-import repast.simphony.relogo.UtilityG;
-import repast.simphony.relogo.schedule.Go;
-import repast.simphony.relogo.schedule.Setup;
-import firesimulation.ReLogoObserver;
+import firesimulation.ReLogoObserver
+import repast.simphony.relogo.schedule.Go
+import repast.simphony.relogo.schedule.Setup
 
-class UserObserver extends ReLogoObserver{
-	def density = 0.5 // TODO move it to project variables
-	
-	@Setup
-	def setup(){
-		clearAll()
-		
-		ask(patches()) {
-			// Set empty space or tree
-			if(Math.random() <= density) {
-				it.setPcolor(green())
-			} else {
-				it.setPcolor(black())
-			}
-		}
-		
-		// Set fire on left side
-		(getMinPycor()..getMaxPycor()).forEach({
-			patch(getMinPxcor(), it).setPcolor(red())
-		})
-	}
+import static repast.simphony.relogo.Utility.*
+import static repast.simphony.relogo.UtilityG.*
 
-	@Go
-	def go(){
-		ask(patches()) {
-			def isTree = it.getPcolor() == green()
-			
-			for(neighbor in it.neighbors()) {
-				if (isTree && neighbor.getPcolor() == red()) {
-					it.setPcolor(red())
-					break;
-				}
-			}
-		}
-		sleep(1000)
-	}
+class UserObserver extends ReLogoObserver {
+
+    @Setup
+    def setup() {
+        clearAll()
+
+        ask(patches()) {
+            initialize()
+        }
+
+        // Set fire on left side
+        (getMinPycor()..getMaxPycor()).forEach({
+            patch(getMinPxcor(), it).setPcolor(red())
+        })
+    }
+
+    @Go
+    def go() {
+        ask(patches()) {
+            makeNextStep()
+        }
+    }
 }
